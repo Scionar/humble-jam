c31ccc41eddbabee42af593bc69cbbb624ed1c1f
Joona Viertola <contact@joonaviertola.com>
2019-04-14 21:46:27 +0300

    Added PostFeedContainer into index page

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,48 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
+import React from "react"
+import PropTypes from "prop-types"
+import { graphql } from "gatsby"
 
-import { PostCard, Pagination } from '../components/common'
-import { MetaData } from '../components/common/meta'
-import { LayoutContainer } from '../containers'
+import { MetaData } from "../components/common/meta"
+import { LayoutContainer, PostFeedContainer } from "../containers"
 
 /**
-* Main index page (home page)
-*
-* Loads all posts from Ghost and uses pagination to navigate through them.
-* The number of posts that should appear per page can be setup
-* in /utils/siteConfig.js under `postsPerPage`.
-*
-*/
-const Index = ({ data, location, pageContext }) => {
-    const posts = data.allGhostPost.edges
-
-    return (
+ * Main index page (home page)
+ *
+ * Loads all posts from Ghost and uses pagination to navigate through them.
+ * The number of posts that should appear per page can be setup
+ * in /utils/siteConfig.js under `postsPerPage`.
+ *
+ */
+const Index = ({ location }) => (
         <>
             <MetaData location={location} />
             <LayoutContainer isHome={true}>
-                <div className="container">
-                    <section className="post-feed">
-                        {posts.map(({ node }) => (
-                            // The tag below includes the markup for each post - components/common/PostCard.js
-                            <PostCard key={node.id} post={node} />
-                        ))}
-                    </section>
-                    <Pagination pageContext={pageContext} />
-                </div>
+                <PostFeedContainer />
             </LayoutContainer>
         </>
-    )
-}
+)
 
 Index.propTypes = {
-    data: PropTypes.shape({
-        allGhostPost: PropTypes.object.isRequired,
-    }).isRequired,
     location: PropTypes.shape({
         pathname: PropTypes.string.isRequired,
     }).isRequired,
-    pageContext: PropTypes.object,
 }
 
 export default Index
@@ -50,17 +33,17 @@ export default Index
 // This page query loads all posts sorted descending by published date
 // The `limit` and `skip` values are used for pagination
 export const pageQuery = graphql`
-  query GhostPostQuery($limit: Int!, $skip: Int!) {
-    allGhostPost(
-        sort: { order: DESC, fields: [published_at] },
-        limit: $limit,
-        skip: $skip
-    ) {
-      edges {
-        node {
-          ...GhostPostFields
+    query GhostPostQuery($limit: Int!, $skip: Int!) {
+        allGhostPost(
+            sort: { order: DESC, fields: [published_at] }
+            limit: $limit
+            skip: $skip
+        ) {
+            edges {
+                node {
+                    ...GhostPostFields
+                }
+            }
         }
-      }
     }
-  }
 `
