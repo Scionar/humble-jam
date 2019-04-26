e2cf0a9256b591bf37fd981303c61a26aa4c394a
Joona Viertola <contact@joonaviertola.com>
2019-04-26 17:42:39 +0300

    Author page feed created with AuthorPostFeedContainer

---
+++ src/containers/AuthorPostFeedContainer.js
@@ -0,0 +1,61 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+import moment from "moment"
+
+import { PostCard } from "../components"
+
+const AuthorPostFeedContainer = ({ data, authorSlug }) => {
+    /*
+     * Filter only current author of this page. This avoids static graphql
+     * striction.
+     */
+    const posts = data.allGhostPost.edges.filter(post => post.node.primary_author.slug === authorSlug)
+
+    return (
+        <>
+            {posts.map(({ node }) => {
+                const postUrl = `/${node.slug}/`
+                const postDate = moment(node.published_at).format(
+                    `MMMM DD, YYYY`
+                )
+                const postTitle = node.title
+                const postImage = node.feature_image
+
+                return (
+                    <PostCard
+                        key={node.id}
+                        postUrl={postUrl}
+                        date={postDate}
+                        title={postTitle}
+                        featuredImage={postImage}
+                    />
+                )
+            })}
+        </>
+    )
+}
+
+AuthorPostFeedContainer.propTypes = {
+    data: PropTypes.object.isRequired,
+    authorSlug: PropTypes.string.isRequired,
+}
+
+const AuthorPostFeedContainerQuery = props => (
+    <StaticQuery
+        query={graphql`
+            query AuthorPostFeedContainerQuery {
+                allGhostPost(sort: { order: DESC, fields: [published_at] }) {
+                    edges {
+                        node {
+                            ...GhostPostFields
+                        }
+                    }
+                }
+            }
+        `}
+        render={data => <AuthorPostFeedContainer data={data} {...props} />}
+    />
+)
+
+export default AuthorPostFeedContainerQuery
--- src/templates/author.js
+++ src/templates/author.js
@@ -1,54 +1,73 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
+import React from "react"
+import PropTypes from "prop-types"
+import { graphql } from "gatsby"
 
-import { PostCard, Pagination } from '../components/common'
-import { MetaData } from '../components/common/meta'
-import { LayoutContainer } from '../containers'
+import { MetaData } from "../components/common/meta"
+import { LayoutContainer, AuthorPostFeedContainer } from "../containers"
 
 /**
-* Author page (/author/:slug)
-*
-* Loads all posts for the requested author incl. pagination.
-*
-*/
-const Author = ({ data, location, pageContext }) => {
+ * Author page (/author/:slug)
+ *
+ * Loads all posts for the requested author.
+ *
+ */
+const Author = ({ data, location }) => {
     const author = data.ghostAuthor
-    const posts = data.allGhostPost.edges
-    const twitterUrl = author.twitter ? `https://twitter.com/${author.twitter.replace(/^@/, ``)}` : null
-    const facebookUrl = author.facebook ? `https://www.facebook.com/${author.facebook.replace(/^\//, ``)}` : null
+    const twitterUrl = author.twitter
+        ? `https://twitter.com/${author.twitter.replace(/^@/, ``)}`
+        : null
+    const facebookUrl = author.facebook
+        ? `https://www.facebook.com/${author.facebook.replace(/^\//, ``)}`
+        : null
 
     return (
         <>
-            <MetaData
-                data={data}
-                location={location}
-                type="profile"
-            />
+            <MetaData data={data} location={location} type="profile" />
             <LayoutContainer>
-                <div className="container">
-                    <header className="author-header">
-                        <div className="author-header-content">
-                            <h1>{author.name}</h1>
-                            {author.bio && <p>{author.bio}</p>}
-                            <div className="author-header-meta">
-                                {author.website && <a className="author-header-item" href={author.website} target="_blank" rel="noopener noreferrer">Website</a>}
-                                {twitterUrl && <a className="author-header-item" href={twitterUrl} target="_blank" rel="noopener noreferrer">Twitter</a>}
-                                {facebookUrl && <a className="author-header-item" href={facebookUrl} target="_blank" rel="noopener noreferrer">Facebook</a>}
-                            </div>
+                <header className="author-header">
+                    <div className="author-header-content">
+                        <h1>{author.name}</h1>
+                        {author.bio && <p>{author.bio}</p>}
+                        <div className="author-header-meta">
+                            {author.website && (
+                                <a
+                                    className="author-header-item"
+                                    href={author.website}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Website
+                                </a>
+                            )}
+                            {twitterUrl && (
+                                <a
+                                    className="author-header-item"
+                                    href={twitterUrl}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Twitter
+                                </a>
+                            )}
+                            {facebookUrl && (
+                                <a
+                                    className="author-header-item"
+                                    href={facebookUrl}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Facebook
+                                </a>
+                            )}
                         </div>
-                        <div className="author-header-image">
-                            {author.profile_image && <img src={author.profile_image} alt={author.name} />}
-                        </div>
-                    </header>
-                    <section className="post-feed">
-                        {posts.map(({ node }) => (
-                            // The tag below includes the markup for each post - components/common/PostCard.js
-                            <PostCard key={node.id} post={node} />
-                        ))}
-                    </section>
-                    <Pagination pageContext={pageContext} />
-                </div>
+                    </div>
+                    <div className="author-header-image">
+                        {author.profile_image && (
+                            <img src={author.profile_image} alt={author.name} />
+                        )}
+                    </div>
+                </header>
+                <AuthorPostFeedContainer authorSlug={author.slug} />
             </LayoutContainer>
         </>
     )
@@ -57,6 +76,7 @@ const Author = ({ data, location, pageContext }) => {
 Author.propTypes = {
     data: PropTypes.shape({
         ghostAuthor: PropTypes.shape({
+            slug: PropTypes.string.isRequired,
             name: PropTypes.string.isRequired,
             cover_image: PropTypes.string,
             profile_image: PropTypes.string,
@@ -71,7 +91,6 @@ Author.propTypes = {
     location: PropTypes.shape({
         pathname: PropTypes.string.isRequired,
     }).isRequired,
-    pageContext: PropTypes.object,
 }
 
 export default Author
@@ -82,14 +101,14 @@ export const pageQuery = graphql`
             ...GhostAuthorFields
         }
         allGhostPost(
-            sort: { order: DESC, fields: [published_at] },
-            filter: {authors: {elemMatch: {slug: {eq: $slug}}}},
-            limit: $limit,
+            sort: { order: DESC, fields: [published_at] }
+            filter: { authors: { elemMatch: { slug: { eq: $slug } } } }
+            limit: $limit
             skip: $skip
         ) {
             edges {
                 node {
-                ...GhostPostFields
+                    ...GhostPostFields
                 }
             }
         }
