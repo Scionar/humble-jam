4cfdb7c5629bd550ecfdb8783dd2201c7a204ab9
Joona Viertola <contact@joonaviertola.com>
2019-04-28 12:14:33 +0300

    Removed author component from author page

--- src/templates/author.js
+++ src/templates/author.js
@@ -13,60 +13,11 @@ import { LayoutContainer, AuthorPostFeedContainer } from "../containers"
  */
 const Author = ({ data, location }) => {
     const author = data.ghostAuthor
-    const twitterUrl = author.twitter
-        ? `https://twitter.com/${author.twitter.replace(/^@/, ``)}`
-        : null
-    const facebookUrl = author.facebook
-        ? `https://www.facebook.com/${author.facebook.replace(/^\//, ``)}`
-        : null
 
     return (
         <>
             <MetaData data={data} location={location} type="profile" />
             <LayoutContainer>
-                <header className="author-header">
-                    <div className="author-header-content">
-                        <h1>{author.name}</h1>
-                        {author.bio && <p>{author.bio}</p>}
-                        <div className="author-header-meta">
-                            {author.website && (
-                                <a
-                                    className="author-header-item"
-                                    href={author.website}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    Website
-                                </a>
-                            )}
-                            {twitterUrl && (
-                                <a
-                                    className="author-header-item"
-                                    href={twitterUrl}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    Twitter
-                                </a>
-                            )}
-                            {facebookUrl && (
-                                <a
-                                    className="author-header-item"
-                                    href={facebookUrl}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    Facebook
-                                </a>
-                            )}
-                        </div>
-                    </div>
-                    <div className="author-header-image">
-                        {author.profile_image && (
-                            <img src={author.profile_image} alt={author.name} />
-                        )}
-                    </div>
-                </header>
                 <AuthorPostFeedContainer authorSlug={author.slug} />
             </LayoutContainer>
         </>
