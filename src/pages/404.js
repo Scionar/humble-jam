ff65a19ea2f98c0177c4e4f0c497b87e2c520048
Joona Viertola <contact@joonaviertola.com>
2019-04-28 13:11:27 +0300

    Stripped down 404 page from old HTML

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,13 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { LayoutContainer } from '../containers'
+import React from "react"
+import { Link } from "gatsby"
+import { LayoutContainer } from "../containers"
 
 const NotFoundPage = () => (
     <LayoutContainer>
-        <div className="container">
-            <article className="content" style={{ textAlign: `center` }}>
-                <h1 className="content-title">Error 404</h1>
-                <section className="content-body">
-                    Page not found, <Link to="/">return home</Link> to start over
-                </section>
-            </article>
-        </div>
+        <h1 className="content-title">Error 404</h1>
+        <section className="content-body">
+            Page not found, <Link to="/">return home</Link> to start over
+        </section>
     </LayoutContainer>
 )
 
