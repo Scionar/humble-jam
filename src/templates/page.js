1ab590694f4b31c5812bed57bc3ac1030fd700ed
Joona Viertola <contact@joonaviertola.com>
2019-05-25 21:49:10 +0300

    Used sharp to transform feature_image on page template

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
+import { get } from 'lodash'
 
 import { PostDate, PostHeroImage, PostContent, MetaData } from "../components"
 import { LayoutContainer } from '../containers'
@@ -14,6 +15,12 @@ import { LayoutContainer } from '../containers'
 const Page = ({ data, location }) => {
     const page = data.ghostPage
 
+    const pageImage = get(
+        page,
+        `localFeatureImage.childImageSharp.fluid.src`,
+        null
+    )
+
     return (
         <>
             <MetaData
@@ -24,8 +31,8 @@ const Page = ({ data, location }) => {
             <LayoutContainer>
                 <PostDate date={page.published_at} />
                 <h1>{page.title}</h1>
-                {page.feature_image && (
-                    <PostHeroImage url={page.feature_image} alt={page.title} />
+                {pageImage && (
+                    <PostHeroImage url={pageImage} alt={page.title} />
                 )}
                 <PostContent html={page.html} />
             </LayoutContainer>
@@ -51,6 +58,13 @@ export const postQuery = graphql`
     query($slug: String!) {
         ghostPage(slug: { eq: $slug }) {
             ...GhostPageFields
+            localFeatureImage {
+                childImageSharp {
+                    fluid(maxWidth: 750, cropFocus: CENTER) {
+                        src
+                    }
+                }
+            }
         }
     }
 `
